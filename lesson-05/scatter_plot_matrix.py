600488f39ba7753a08d3ba2776001a04bfa2f18b
mtraina <matteo.traina.mail@gmail.com>
2018-07-08 10:24:55 +0200

    lesson 5

---
+++ lesson-05/scatter_plot_matrix.py
@@ -0,0 +1,13 @@
+# Scatter Plot Matrix
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+
+url = 'https://goo.gl/bDdBiA'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = read_csv(url, names=names)
+scatter_matrix(data)
+plt.show()
